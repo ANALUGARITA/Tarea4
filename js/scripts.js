31679411cff3e46ed880ea1f38d42dba87eee718
¨ANALUGARITA¨ <analugarita@gmail.com>
2021-06-14 22:53:07 -0600

    second

--- js/scripts.js
+++ js/scripts.js
@@ -24,27 +24,25 @@
         "OSM": osm, 
         "Esri_World": Esri_WorldGrayCanvas,
 	};	    
-	    
-// Control de capas
-    control_capas = L.control.layers(mapasBase).addTo(mapa);
-	
-// Control de escala
-    L.control.scale({ position: 'bottomright', imperial: false }).addTo(mapa);       
-
+  
 // Ícono personalizado para cafe
     const iconoCafe = L.divIcon({
         html: '<i class="fas fa-mug-hot fa-2x"></i>',
         className: 'estiloIconos'
 });
 
-
+// Control de capas
+    control_capas = L.control.layers(mapasBase).addTo(mapa);
+	
+// Control de escala
+    L.control.scale({ position: 'bottomright', imperial: false }).addTo(mapa);       
 
 // Capa vectorial en formato GeoJSON
-$.getJSON("https://raw.githubusercontent.com/ANALUGARITA/Tarea3/main/capas/fincascafe.geojson", function(geodata) {
+$.getJSON("https://raw.githubusercontent.com/ANALUGARITA/Tarea4/main/capas/fincascafe.geojson", function(geodata) {
   // Registros individuales
   var capa_fincascafe = L.geoJson(geodata, {
     style: function(feature) {
-	  return {'color': "#013220", 'weight': 3}
+	  return {'color': "#011a32", 'weight': 3}
     },
     onEachFeature: function(feature, layer) {
       var popupText = "<strong>Finca</strong>: " + feature.properties.Nombre + "<br>" + 
@@ -53,22 +51,35 @@ $.getJSON("https://raw.githubusercontent.com/ANALUGARITA/Tarea3/main/capas/finca
       layer.bindPopup(popupText);
     },
     pointToLayer: function(getJsonPoint, latlng) {
-    return L.marker(latlng, {icon: iconoCafe});
+        return L.marker(latlng, {icon: iconoCafe});
     }
   });
 
+  // Capa de calor (heatmap)
+  coordenadas = geodata.features.map(feat => feat.geometry.coordinates.reverse());
+  var capa_cafe_calor = L.heatLayer(coordenadas, {
+                                                    radius: 60,
+                                                    blur: 1, 
+                                                    gradient: {
+                                                        0.0: 'blue', 
+                                                        1.0: 'red'
+                                                    }
+                                                 }).addTo(mapa);
+
   // Capa de puntos agrupados
-  var capa_cafe_agrupados = L.markerClusterGroup();
+  var capa_cafe_agrupados = L.markerClusterGroup({spiderfyOnMaxZoom: true});
   capa_cafe_agrupados.addLayer(capa_fincascafe);
 
   // Se añade la capa al mapa y al control de capas
   capa_cafe_agrupados.addTo(mapa);
+  capa_cafe_calor.addTo(mapa);
+  control_capas.addOverlay(capa_cafe_calor, 'Mapa de calor cafe');  
   control_capas.addOverlay(capa_cafe_agrupados, 'Fincas agrupadas de cafe');
   control_capas.addOverlay(capa_fincascafe, 'Fincas de cafe');
 });
 
 
-$.getJSON("https://raw.githubusercontent.com/ANALUGARITA/Tarea3/main/capas/viascafe.geojson", function(geodata) {
+$.getJSON("https://raw.githubusercontent.com/ANALUGARITA/Tarea4/main/capas/viascafe.geojson", function(geodata) {
   var capa_viascafe = L.geoJson(geodata, {
     style: function(feature) {
 	  return {'color': "red", 'weight': 1.5, 'fillOpacity': 0.0}
@@ -82,7 +93,7 @@ $.getJSON("https://raw.githubusercontent.com/ANALUGARITA/Tarea3/main/capas/viasc
   control_capas.addOverlay(capa_viascafe, 'Vías a Fincas'); 
 });
 
-$.getJSON("https://raw.githubusercontent.com/ANALUGARITA/Tarea2/main/capas/cantonescafe.geojson", function(geodata) {
+$.getJSON("https://raw.githubusercontent.com/ANALUGARITA/Tarea4/main/capas/cantonescafe.geojson", function(geodata) {
   var capa_cantones = L.geoJson(geodata, {
     style: function(feature) {
 	  return {'color': "black", 'weight': 1.5, 'fillOpacity': 0.0}
